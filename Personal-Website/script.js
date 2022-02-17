8124984ff27e6229da1550e71cab6d222aff0b14
Austin Shaw <austin@bioliteenergy.com>
2022-02-17 15:59:59 -0500

    Working on implementing slider in bio section

---
+++ Personal-Website/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+const dotContainer = document.querySelector('.dots');
+
+btnLeft.addEventListener('click', function (e) {});
+btnRight.addEventListener('click', function (e) {});
+
+const createDots = function () {
+  slides.forEach(function (_, i) {
+    dotContainer.insertAdjacentHTML(
+      'beforeend',
+      `<button class="dots__dot" data-slide="${i}"></button>`
+    );
+  });
+};
+
+const activateDot = function (slide) {
+  document
+    .querySelectorAll('.dots__dot')
+    .forEach(dot => dot.classList.remove('dots__dot--active'));
+
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add('dots__dot--active');
+};
+
+createDots();
