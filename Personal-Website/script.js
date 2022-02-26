81317aa3193d787f90815b7e0990e08e9ad7bb1b
Austin Shaw <ashaw121@gmail.com>
2022-02-26 15:22:56 -0500

    Trying to use better design principles, changed font, added color, trying to add hero with gradient but having issues getting the sizing right.

--- Personal-Website/script.js
+++ Personal-Website/script.js
@@ -1,79 +1,79 @@
 'use strict';
 
-const initialize = function () {
-  createDots();
-  activateDot(0);
-  goToSlide(0);
-};
+// const initialize = function () {
+//   createDots();
+//   activateDot(0);
+//   goToSlide(0);
+// };
 
-const slides = document.querySelectorAll('.slide');
-const btnLeft = document.querySelector('.slider__btn--left');
-const btnRight = document.querySelector('.slider__btn--right');
-const dotContainer = document.querySelector('.dots');
+// const slides = document.querySelectorAll('.slide');
+// const btnLeft = document.querySelector('.slider__btn--left');
+// const btnRight = document.querySelector('.slider__btn--right');
+// const dotContainer = document.querySelector('.dots');
 
-let curSlide = 0;
-const maxSlide = slides.length - 1;
+// let curSlide = 0;
+// const maxSlide = slides.length - 1;
 
-const createDots = function () {
-  slides.forEach(function (_, i) {
-    dotContainer.insertAdjacentHTML(
-      'beforeend',
-      `<button class="dots__dot" data-slide="${i}"></button>`
-    );
-  });
-};
+// const createDots = function () {
+//   slides.forEach(function (_, i) {
+//     dotContainer.insertAdjacentHTML(
+//       'beforeend',
+//       `<button class="dots__dot" data-slide="${i}"></button>`
+//     );
+//   });
+// };
 
-const activateDot = function (slide) {
-  document
-    .querySelectorAll('.dots__dot')
-    .forEach(dot => dot.classList.remove('dots__dot--active'));
+// const activateDot = function (slide) {
+//   document
+//     .querySelectorAll('.dots__dot')
+//     .forEach(dot => dot.classList.remove('dots__dot--active'));
 
-  document
-    .querySelector(`.dots__dot[data-slide="${slide}"]`)
-    .classList.add('dots__dot--active');
-};
+//   document
+//     .querySelector(`.dots__dot[data-slide="${slide}"]`)
+//     .classList.add('dots__dot--active');
+// };
 
-const goToSlide = function (slide) {
-  slides.forEach(
-    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
-  );
-};
+// const goToSlide = function (slide) {
+//   slides.forEach(
+//     (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+//   );
+// };
 
-const nextSlide = function () {
-  if (curSlide === maxSlide) {
-    curSlide = 0;
-  } else {
-    curSlide++;
-  }
+// const nextSlide = function () {
+//   if (curSlide === maxSlide) {
+//     curSlide = 0;
+//   } else {
+//     curSlide++;
+//   }
 
-  goToSlide(curSlide);
-  activateDot(curSlide);
-};
+//   goToSlide(curSlide);
+//   activateDot(curSlide);
+// };
 
-const prevSlide = function () {
-  if (curSlide === 0) {
-    curSlide = maxSlide;
-  } else {
-    curSlide--;
-  }
+// const prevSlide = function () {
+//   if (curSlide === 0) {
+//     curSlide = maxSlide;
+//   } else {
+//     curSlide--;
+//   }
 
-  goToSlide(curSlide);
-  activateDot(curSlide);
-};
+//   goToSlide(curSlide);
+//   activateDot(curSlide);
+// };
 
-initialize();
+// initialize();
 
-btnLeft.addEventListener('click', prevSlide);
-btnRight.addEventListener('click', nextSlide);
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'ArrowLeft') prevSlide();
-  if (e.key === 'ArrowRight') nextSlide();
-});
+// btnLeft.addEventListener('click', prevSlide);
+// btnRight.addEventListener('click', nextSlide);
+// document.addEventListener('keydown', function (e) {
+//   if (e.key === 'ArrowLeft') prevSlide();
+//   if (e.key === 'ArrowRight') nextSlide();
+// });
 
-dotContainer.addEventListener('click', function (e) {
-  if (e.target.classList.contains('dots__dot')) {
-    const { slide } = e.target.dataset;
-    goToSlide(slide);
-    activateDot(slide);
-  }
-});
+// dotContainer.addEventListener('click', function (e) {
+//   if (e.target.classList.contains('dots__dot')) {
+//     const { slide } = e.target.dataset;
+//     goToSlide(slide);
+//     activateDot(slide);
+//   }
+// });
